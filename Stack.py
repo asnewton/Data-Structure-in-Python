89c82e12729a18f3b56a6d254eae93206436c1b2
Ahmad Ashraf <ahmadashrafdgp@gmail.com>
2017-12-31 02:19:37 -0600

    Add files via upload
    This is how to create stack.

---
+++ Stack.py
@@ -0,0 +1,50 @@
+class NewStack:
+
+    stack = []
+
+    """initialize satck"""
+    def __init__(self):
+        self.stack = []
+
+    """check weather stack is empty or note"""
+    def isempty(self):
+        if self.stack == []:
+            return True
+        return False
+
+    """insert an item into stack"""
+    def push(self, item):
+        self.stack.append(item)
+
+    """delete last element and return it"""
+    def pop(self):
+        return self.stack.pop()
+
+    """returns the last element"""
+    def peek(self):
+        return self.stack[len(self.stack) - 1]
+
+    """returns the size of stack"""
+    def size(self):
+        return len(self.stack)
+
+    """print the element of stack"""
+    def printstack(self):
+        return self.stack
+
+
+"""operations on stack
+stack_1 = NewStack()
+stack_1.push('Ahmad')
+stack_1.push(1998)
+stack_1.push('Falak')
+print(stack_1.size())
+print(stack_1.isempty())
+print(stack_1.peek())
+print(stack_1.size())
+print(stack_1.printstack())
+stack_1.push('Adaa')
+stack_1.push('2000')
+print(stack_1.printstack())
+print(stack_1.pop())
+"""
